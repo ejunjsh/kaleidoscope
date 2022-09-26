c49187389b25237063542defd37dfb2b2330e887
zhouff <sjj050121014@163.com>
2022-09-26 11:41:46 +0800

    add cn comment on lib.cc

--- lib.cc
+++ lib.cc
@@ -1,5 +1,6 @@
 //===----------------------------------------------------------------------===//
 // "Library" functions that can be "extern'd" from user code.
+// 库函数，可以由用户代码调用的函数
 //===----------------------------------------------------------------------===//
 #include <cstdio>
 
@@ -10,12 +11,14 @@
 #endif
 
 /// putchard - putchar that takes a double and returns 0.
+// 打印一个字符
 extern "C" DLLEXPORT double putchard(double X) {
   fputc((char)X, stderr);
   return 0;
 }
 
 /// printd - printf that takes a double prints it as "%f\n", returning 0.
+// 打印一个双精度浮点数
 extern "C" DLLEXPORT double printd(double X) {
   fprintf(stderr, "%f\n", X);
   return 0;
