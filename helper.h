4b8c02345974724f1c9c855c230781fbbc269f46
zhouff <sjj050121014@163.com>
2022-10-01 00:50:34 +0800

    add cn comments

--- helper.h
+++ helper.h
@@ -2,6 +2,7 @@
 #define HELPER_H
 
 /// LogError* - These are little helper functions for error handling.
+/// 错误日志输出
 inline std::unique_ptr<ExprAST> LogError(const char *Str) {
   fprintf(stderr, "Error: %s\n", Str);
   return nullptr;
