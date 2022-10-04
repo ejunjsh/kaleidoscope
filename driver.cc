db07395d2305967037a74e3b981b54fd9f1d7866
zhouff <sjj050121014@163.com>
2022-10-04 15:44:17 +0800

    add cn comments on driver.cc

--- driver.cc
+++ driver.cc
@@ -42,21 +42,27 @@
 
 //===----------------------------------------------------------------------===//
 // Top-Level parsing and JIT Driver
+// 顶层解析和JIT 驱动
 //===----------------------------------------------------------------------===//
 
+// 初始化一个模块和优化
 void InitializeModuleAndPassManager() {
   // Open a new module.
+  // 打开一个新模块
   TheContext = std::make_unique<LLVMContext>();
   TheModule = std::make_unique<Module>("my cool jit", *TheContext);
   TheModule->setDataLayout(TheJIT->getDataLayout());
 
   // Create a new builder for the module.
+  // 创建一个代码生成构造器给这个模块
   Builder = std::make_unique<IRBuilder<>>(*TheContext);
 
   // Create a new pass manager attached to it.
+  // 创建一个优化器
   TheFPM = std::make_unique<legacy::FunctionPassManager>(TheModule.get());
 
   // Promote allocas to registers.
+  // 提升变量存储（栈）到寄存器 
   TheFPM->add(createPromoteMemoryToRegisterPass());
   // Do simple "peephole" optimizations and bit-twiddling optzns.
   TheFPM->add(createInstructionCombiningPass());
@@ -70,6 +76,7 @@ void InitializeModuleAndPassManager() {
   TheFPM->doInitialization();
 }
 
+// 解析和代码生成函数定义
 void HandleDefinition() {
   if (auto FnAST = ParseDefinition()) {
     if (auto *FnIR = FnAST->codegen()) {
@@ -82,10 +89,12 @@ void HandleDefinition() {
     }
   } else {
     // Skip token for error recovery.
+    // 错误恢复
     getNextToken();
   }
 }
 
+// 解析和代码生成函数声明（原型函数）
 void HandleExtern() {
   if (auto ProtoAST = ParseExtern()) {
     if (auto *FnIR = ProtoAST->codegen()) {
@@ -96,16 +105,20 @@ void HandleExtern() {
     }
   } else {
     // Skip token for error recovery.
+    // 错误恢复
     getNextToken();
   }
 }
 
+// 解析顶层表达式，并JIT运行
 void HandleTopLevelExpression() {
   // Evaluate a top-level expression into an anonymous function.
+  // 解析顶层表达式到一个匿名函数
   if (auto FnAST = ParseTopLevelExpr()) {
     if (FnAST->codegen()) {
       // Create a ResourceTracker to track JIT'd memory allocated to our
       // anonymous expression -- that way we can free it after executing.
+      // 创建这个方便执行后释放
       auto RT = TheJIT->getMainJITDylib().createResourceTracker();
 
       auto TSM = ThreadSafeModule(std::move(TheModule), std::move(TheContext));
@@ -113,30 +126,35 @@ void HandleTopLevelExpression() {
       InitializeModuleAndPassManager();
 
       // Search the JIT for the __anon_expr symbol.
+      // 搜索是否有__anon_expr这个符号
       auto ExprSymbol = ExitOnErr(TheJIT->lookup("__anon_expr"));
 
       // Get the symbol's address and cast it to the right type (takes no
       // arguments, returns a double) so we can call it as a native function.
+      // 查找这个函数地址，并像执行普通函数一样执行它
       double (*FP)() = (double (*)())(intptr_t)ExprSymbol.getAddress();
       fprintf(stderr, "Evaluated to %f\n", FP());
 
       // Delete the anonymous expression module from the JIT.
+      // 从JIT里面，删除包含这个匿名函数的模块
       ExitOnErr(RT->remove());
     }
   } else {
     // Skip token for error recovery.
+    // 错误恢复
     getNextToken();
   }
 }
 
 /// top ::= definition | external | expression | ';'
+/// 主循环
 void MainLoop() {
   while (true) {
     fprintf(stderr, "ready> ");
     switch (CurTok) {
     case tok_eof:
       return;
-    case ';': // ignore top-level semicolons.
+    case ';': // ignore top-level semicolons. 跳过分号
       getNextToken();
       break;
     case tok_def:
@@ -157,7 +175,7 @@ void MainLoop() {
 //===----------------------------------------------------------------------===//
 // Main driver code.
 //===----------------------------------------------------------------------===//
-
+// 入口
 int main() {
   InitializeNativeTarget();
   InitializeNativeTargetAsmPrinter();
@@ -165,13 +183,16 @@ int main() {
 
   // Install standard binary operators.
   // 1 is lowest precedence.
+  // 安装标准二元操作符
+  // 1是最低的优先级
   BinopPrecedence['='] = 2;
   BinopPrecedence['<'] = 10;
   BinopPrecedence['+'] = 20;
   BinopPrecedence['-'] = 20;
-  BinopPrecedence['*'] = 40; // highest.
+  BinopPrecedence['*'] = 40; // highest. 最高优先级
 
   // Prime the first token.
+  // 开始读取关键字，这时候定位在一个关键字
   fprintf(stderr, "ready> ");
   getNextToken();
 
@@ -180,6 +201,7 @@ int main() {
   InitializeModuleAndPassManager();
 
   // Run the main "interpreter loop" now.
+  // 运行循环，不断编译
   MainLoop();
 
   return 0;
